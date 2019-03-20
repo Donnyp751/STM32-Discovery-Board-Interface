acb5288561aa0c2c4e627244fd8686cb6e2487fc
Donald Posterick <donny.p@live.com>
2019-03-20 12:01:21 -0500

    Added project, laying out UI. Added ability to see available com ports.

---
+++ Control Panel/Control Panel/Connection.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace Control_Panel
+{
+    class Connection : INotifyPropertyChanged
+    {
+        private string _connectionStatus;
+        private ObservableCollection<string> comPorts;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string ConnectionStatus
+        {
+            get => _connectionStatus;
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _connectionStatus = value;
+                    NotifyPropertyChanged();
+                    //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ConnectionStatus"));
+                }
+                    
+            }
+        }
+
+        public ObservableCollection<string> ComPorts
+        {
+            get => comPorts;
+            set
+            {
+                if (value != null)
+                {
+                    comPorts = value;
+                    NotifyPropertyChanged();
+                    //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ComPorts"));
+                }
+            }
+
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public Connection()
+        {
+            ConnectionStatus = "Disconnected";
+            updateComPorts();
+        }
+
+        private void updateComPorts()
+        {
+            ComPorts = new ObservableCollection<string>(SerialPort.GetPortNames());
+        }
+
+        public void ChangeConnectionStatus(string connection)
+        {
+            ConnectionStatus = connection;
+        }
+
+
+    }
+}
---
+++ Control Panel/Control Panel/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Control_Panel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Connection connection;
+        public MainWindow()
+        {
+            InitializeComponent();
+            connection = new Connection();
+
+            ConnectionTab.DataContext = connection;
+            //ConnectionStatusTextBlock.DataContext = connection;
+           
+        }
+        
+        private void ConnectBtnClick(object sender, RoutedEventArgs e)
+        {
+            connection.ChangeConnectionStatus("Connected");
+        }
+    }
+}
