029088311b89af04c04f98e3616787af5cc11b2e
Donald Posterick <donny.p@live.com>
2019-03-22 18:27:20 -0500

    Got the live sensor data plots updating from the embedded system

--- Control Panel/Control Panel/MainWindow.xaml.cs
+++ Control Panel/Control Panel/MainWindow.xaml.cs
@@ -26,18 +26,25 @@ namespace Control_Panel
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static readonly DependencyProperty TemperatureDataProperty =
+            DependencyProperty.Register("TemperatureData", typeof(ObservableCollection<DataPoint>), typeof(MainWindow), new PropertyMetadata(new ObservableCollection<DataPoint>()));
+        public static readonly DependencyProperty HumidityDataProperty =
+            DependencyProperty.Register("HumidityData", typeof(ObservableCollection<DataPoint>), typeof(MainWindow), new PropertyMetadata(new ObservableCollection<DataPoint>()));
+
+        private float latestTemperatureReading = 0;
+        private float latestHumidityReading = 0;
         private DispatcherTimer updateTimer = new DispatcherTimer();
         private Connection connection;
         private double time = 0;
         public ObservableCollection<DataPoint> TemperatureData
         {
-            get { return (ObservableCollection<DataPoint>)GetValue(DataProperty); }
-            set { SetValue(DataProperty, value); }
+            get { return (ObservableCollection<DataPoint>)GetValue(TemperatureDataProperty); }
+            set { SetValue(TemperatureDataProperty, value); }
         }
         public ObservableCollection<DataPoint> HumidityData
         {
-            get { return (ObservableCollection<DataPoint>)GetValue(DataProperty); }
-            set { SetValue(DataProperty, value); }
+            get { return (ObservableCollection<DataPoint>)GetValue(HumidityDataProperty); }
+            set { SetValue(HumidityDataProperty, value); }
         }
         public MainWindow()
         {
@@ -45,11 +52,22 @@ namespace Control_Panel
             connection = new Connection();
             MainGrid.DataContext = connection;
             ConsoleTb.TextChanged += ConsoleOutput_TextChanged;
+            SensorDataDisplayGrid.DataContext = this;
             updateTimer.Interval = TimeSpan.FromMilliseconds(1000);
+            updateTimer.Tick += UpdateTimer_Tick;
+
             connection.OnSerialMessageReceived += Connection_OnSerialMessageReceived;
 
         }
 
+        private void UpdateTimer_Tick(object sender, EventArgs e)
+        {
+            TemperatureData.Add(new DataPoint(time, latestTemperatureReading));
+            HumidityData.Add(new DataPoint(time, latestHumidityReading));
+            time++;
+        }
+
+        private char[] numbers = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'};
         private void parseTempAndHumid(string message, out float temp, out float humid)
         {
             string[] splitMessage = message.Split('\t');
@@ -59,8 +77,12 @@ namespace Control_Panel
             {
                 if (part.Contains("temperature"))
                 {
-                    string tempStr = part.Remove(0, 21);
-                    tempStr = tempStr.TrimEnd(new char[] {'C', ' '});
+                    var splitEnd = part.IndexOfAny(numbers, part.Length/2);
+                    string tempStr = part.Remove(0, splitEnd);
+
+                    //tempStr = part.Remove(0, part.IndexOfAny(numbers,0)-1);
+                    tempStr = tempStr.Remove(tempStr.LastIndexOfAny(numbers),
+                       tempStr.Length - tempStr.LastIndexOfAny(numbers));
 
                     temp = (float) Convert.ToDouble(tempStr);
                 }else if (part.Contains("humidity"))
@@ -74,8 +96,8 @@ namespace Control_Panel
         private void Connection_OnSerialMessageReceived(object sender, MessageEventArgs args)
         {
             parseTempAndHumid(args.Message, out float temp,out float humid );
-            TemperatureData.Add(new DataPoint(time, temp));
-            HumidityData.Add(new DataPoint(time, humid));
+            latestTemperatureReading = temp;
+            latestHumidityReading = humid;
         }
 
         private void ConsoleOutput_TextChanged(object sender, TextChangedEventArgs e)
@@ -92,17 +114,20 @@ namespace Control_Panel
                 if (!string.IsNullOrEmpty(comPort))
                 {
                     if (connection.Connect(comPort))
+                    {
+                        updateTimer.Start();
                         ConnectButton.Content = "Disconnect";
+                    }
                 }
             }
             else
             {
+                updateTimer.Stop();
                 connection.Disconnect();
                 ConnectButton.Content = "Connect";
+                
             }
         }
-        public static readonly DependencyProperty DataProperty =
-            DependencyProperty.Register("Data", typeof(ObservableCollection<DataPoint>), typeof(MainWindow), new PropertyMetadata(new ObservableCollection<DataPoint>()));
 
 
     }
